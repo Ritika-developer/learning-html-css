ac7fd4238f705ad434afa4c46be47c5da09d6b65
Ritika-developer <ritikachoudhary6264@gmail.com>
2025-02-16 17:42:12 +0530

    js

---
+++ 15feb/jscript.js
@@ -0,0 +1,82 @@
+// let a=parseInt(prompt("enter a number"))
+
+// if(a>0)
+// {
+//     console.log("positive")
+// }
+
+// else if (a<0)
+// {
+//     console.log("negative")
+// }
+// else{
+//     console.log("nutral")
+// }
+
+
+// let a=parseInt(prompt("enter firsrt subject number"))
+// let b=parseInt(prompt("enter second subject number"))
+// let c=parseInt(prompt("enter third subject number"))
+// let mark=(a+b+c/300)*100
+// if(mark>90){
+//     console.log("grade A")
+// }
+
+// else if (mark>=75 && mark<90){
+//     console.log("grade B")
+// }
+
+// else if(mark>=60 && mark<75){
+//     console.log("grade C")
+// }
+// else{
+//     console.log("fail")
+// }
+
+
+
+
+
+// if (condition){
+//     if(condition){
+
+//     }
+//     else{
+
+//     }
+// }
+// else{
+
+// }
+
+//nested if 
+
+// let age=parseInt(prompt("enter your age"))
+
+// if(age>=18){
+
+// let a=parseInt(prompt("press 1 for indian"))
+// if (a==1){
+//     console.log("eligible")
+// }
+// else{
+//     console.log("not eligible")
+// }
+
+// }
+// else{
+//     console.log("you are child")
+// }
+
+
+
+let a=parseInt(prompt("enter a number"))
+
+switch(a){
+    case 1 : console.log("ritika")
+    break
+    case 2 : console.log("saloni")
+    break
+    case 3 : console.log("shivai")
+    break
+}
