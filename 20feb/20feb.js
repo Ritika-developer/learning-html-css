b3e98c694a3e82a21f9730e6f14a8c11e6bdb627
Ritika-developer <ritikachoudhary6264@gmail.com>
2025-02-20 15:57:19 +0530

    20feb

---
+++ 20feb/20feb.js
@@ -0,0 +1,16 @@
+// no argument && return
+
+// function google ()
+// {
+//     return "company"
+// }
+
+// // console.log(google())
+
+// let nm=google()
+// document.write(nm)
+
+
+
+
+                // -: DOM :- = document object model
